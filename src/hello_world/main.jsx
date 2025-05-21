5559b3a6f81bd11fe960ed7191be04b6bc1847b6
Daffa <mdaffakha@gmail.com>
2025-05-21 18:53:45 +0700

    add Container.jsx dan update untuk main.js

--- src/hello_world/main.jsx
+++ src/hello_world/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import HelloWorld from "./HelloWorld";
 import { StrictMode } from "react";
+import Container from "./Container";
 
 // createRoom dibuat untuk memanggil component yang akan dimunculkan
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         {/* component ini bisa digunakan berkali kali jadi reusable */}
-        <HelloWorld/>
+        <Container>
+            <HelloWorld />
+        </Container>
     </StrictMode>
 )
