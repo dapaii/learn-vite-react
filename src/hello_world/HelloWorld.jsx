1a6a6c93d09a7fe465c127dbc1939a79edb84093
Daffa <mdaffakha@gmail.com>
2025-05-21 17:32:57 +0700

    add multiple componen on HelloWorld.jsx, update 2 func

--- src/hello_world/HelloWorld.jsx
+++ src/hello_world/HelloWorld.jsx
@@ -3,11 +3,29 @@ export default function HelloWorld(){
         // di jsx ketika membuat elemen html harus di bungkus dengan satu elemen
         // jika tidak di bungkus maka akan error
         <div>
-            <h1>HelloWorld</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nihil, ipsa culpa eius pariatur eveniet! Quidem excepturi doloribus alias aspernatur?</p>
+            <HeaderHelloWorld/>
+            <ParagraphHelloWorld/>
         </div>
     )
 }
+
+// multiple component START
+function HeaderHelloWorld() {
+    return(
+        <>
+            <h1>Hello World</h1>
+        </>
+    )
+}
+
+function ParagraphHelloWorld(){
+    return(
+        <>
+            <p>Belajar React pemula</p>
+        </>
+    )
+}
+// multiple component END
 
 // component hello world bisa dibuat seperti ini juga
 // function HelloWorld(){
