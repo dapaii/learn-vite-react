c08d984cb5f8a9ea7306cea25365a5663dc06c37
Daffa <mdaffakha@gmail.com>
2025-05-21 16:08:28 +0700

    add hello_world HelloWorld on assets

---
+++ src/assets/hello_world/HelloWorld.jsx
@@ -0,0 +1,18 @@
+export default function HelloWorld(){
+    return(
+        // di jsx ketika membuat elemen html harus di bungkus dengan satu elemen
+        // jika tidak di bungkus maka akan error
+        <div>
+            <h1>HelloWorld</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nihil, ipsa culpa eius pariatur eveniet! Quidem excepturi doloribus alias aspernatur?</p>
+        </div>
+        
+    )
+}
+
+// component hello world bisa dibuat seperti ini juga
+// function HelloWorld(){
+
+// }
+
+// export default HelloWorld;
