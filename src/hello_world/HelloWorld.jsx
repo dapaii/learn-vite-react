46e31631107134ff2581f9f53d69418e585b268a
Daffa <mdaffakha@gmail.com>
2025-05-21 18:43:33 +0700

    update HelloWorld.tsx add props (destructuring, spread)

--- src/hello_world/HelloWorld.jsx
+++ src/hello_world/HelloWorld.jsx
@@ -1,18 +1,27 @@
 export default function HelloWorld(){
-    return(
+        // misal props ini sudah ada seperti ini
+        const props = {
+            props: "Spread syntax to learn react"
+        }
+    return(    
         // di jsx ketika membuat elemen html harus di bungkus dengan satu elemen
         // jika tidak di bungkus maka akan error
         <div>
-            <HeaderHelloWorld/>
-            <ParagraphHelloWorld/>
+            {/* kita mengirim props text-"Hello world" karena sudah ada props ini maka "tidak ada props" tidak dipakai */}
+            <HeaderHelloWorld text="Hello World"/>
+            {/* menggunakan props spread */}
+            <ParagraphHelloWorld {...props}/>
         </div>
     )
 }
 
 // multiple component START
-function HeaderHelloWorld() {
+// pada parameter kita akan menambahkan props yang dikirimkan ke component, tidak wajib namanya props. Tidak wajib juga diisi
+
+// kita tambahkan destructuring untuk mempermudah pengambilan props pada parameter => (props) => ({text = "Hello World"}) sifatnya sementara saja jika tidak ada props yang dikirimkan maka akan menggunakan default value "tidak ada props"
+
+function HeaderHelloWorld({text = "tidak ada props"}){
     // JsOnJSX START
-    const text = "Hello World"
     return(
         <>
         {/* terlihat seperti double kurawal tapi sebenarnya tidak, kurawal tetap 1 tetapi yang satunya lagi itu object untuk style di jsx menggunakan css */}
@@ -29,14 +38,12 @@ function HeaderHelloWorld() {
     )
 }
 
-function ParagraphHelloWorld(){
-    const text = "Belajar react pemula"
+function ParagraphHelloWorld({text = "tidak ada props"}){
     // perbedaan implementasi pada style untuk object yang dipanggil
     const style = {
         color: "blue",
         backgroundColor: "yellow"
     }
-
     return(
         <>
             <p style={style}>{text.toLowerCase()}</p>
