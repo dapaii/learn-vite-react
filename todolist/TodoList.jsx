0994a884b5d474d7375e3528651865f31897dce0
Daffa <mdaffakha@gmail.com>
2025-05-22 16:52:11 +0700

    update commentar pada todolist

--- todolist/TodoList.jsx
+++ todolist/TodoList.jsx
@@ -3,6 +3,7 @@ import Todo from './Todo.jsx';
 export default function TodoList(){
     return(
         <ul>
+            {/* masukan contional dengan props yang tersedia di todolist */}
             <Todo text="Learn React" isCompleted={false}/>
             <Todo text="Learn JavaScript" isCompleted={true}/>
             <Todo text="Learn CSS" isCompleted={false}/>
