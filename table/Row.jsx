d2a251f4db5cb43bc283323a80519ea69a713ef9
Daffa <mdaffakha@gmail.com>
2025-05-23 17:55:14 +0700

    add table(row, table) creaete pure component

--- table/Row.jsx
+++ table/Row.jsx
@@ -1,11 +1,8 @@
-let counter = 0;
-// This component is used to display a row in the table
 
-export default function Row({text}){
-  counter++;
+export default function Row({id,text}){
   return(
     <tr>
-      <td>{counter}</td>
+      <td>{id}</td>
       <td>{text}</td>
     </tr>
   )
--- table/Table.jsx
+++ table/Table.jsx
@@ -5,9 +5,9 @@ export default function Table(){
   return(
     <table border="1">
       <tbody>
-        <Row text="satu"/>
-        <Row text="dua"/>
-        <Row text="tiga"/>
+        <Row id="1" text="satu"/>
+        <Row id="2" text="dua"/>
+        <Row id="3" text="tiga"/>
       </tbody>
     </table>
   )
