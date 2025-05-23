e63b09d5f0a52a587c1a6246b1b56c0ba4bb9e1b
Daffa <mdaffakha@gmail.com>
2025-05-23 17:40:13 +0700

    update totolist add component key

--- todolist/TodoList.jsx
+++ todolist/TodoList.jsx
@@ -3,29 +3,41 @@ import Todo from "./Todo.jsx";
 export default function TodoList() {
   const data = [
     {
+      // add komponent id for component key
+      id: 1,
       text: "Learn React",
       isCOmpleted: false
     },
       {
+        id: 2,
         text: "Learn JavasScript",
         isCompleted: true
       },
       {
+        id: 3,
         text: "Learn CSS",
         isCompleted: false,
       },
       {
+        id: 4,
         text: "Learn HTML",
+        isCompleted: true
+      },
+      {
+        id: 5,
+        text: "Learn NodeJS",
         isCompleted: true
       }
   ]
 
-  const todos = data.map((todo) => <Todo {...todo}/>)
-    
+  const todos = data.map((todo) => <Todo key={todo.id} {...todo}/>)
     return (
       <ul>
         {/* collection components */}
-        {todos}
+        {/* update component for component key */}
+        {data.map((todo) => (
+          <Todo key={todo.id} {...todo}/>
+        ))}
       </ul>
     );
 }
