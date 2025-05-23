7d550894d40d8ddd40d04296ebc7b0c852e33968
Daffa <mdaffakha@gmail.com>
2025-05-23 18:17:34 +0700

    update allertbutton event handler sebagai props

--- src/hello_world/main.jsx
+++ src/hello_world/main.jsx
@@ -4,7 +4,8 @@ import { StrictMode } from "react";
 import Container from "./Container";
 import TodoList from "../../todolist/TodoList";
 import Table from "../../table/Table";
-import AllertButton from "../../button/AllertButton";
+import AllertButton from "../button/AllertButton";
+import MyButton from "../button/MyButton";
 
 // createRoom dibuat untuk memanggil component yang akan dimunculkan
 createRoot(document.getElementById("root")).render(
@@ -15,6 +16,8 @@ createRoot(document.getElementById("root")).render(
       <TodoList />
       <Table />
       <AllertButton text="click me" message="you click me" />
+      <MyButton text="Smash me" onSmash={() => alert("you smash me")}/>
+      <MyButton text="kick me" onSmash={() => alert("you kick me")}/>
     </Container>
   </StrictMode>
 );
