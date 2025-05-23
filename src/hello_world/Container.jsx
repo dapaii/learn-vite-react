53a406643543e37be3361d0a25be0e9604a96a6f
Daffa <mdaffakha@gmail.com>
2025-05-23 15:18:20 +0700

    update formatter

--- src/hello_world/Container.jsx
+++ src/hello_world/Container.jsx
@@ -1,14 +1,14 @@
 // Nested Component children jadi memungkinkan kita untuk memanggil component lain di dalam component, dan penulisannya juga memang harus children pada parameter
 // periksa di src/main.jsx kita akan memanggil component Container
-export default function Container({children}){
-    return(
-        <div>
-            <h2>Muhamad Daffa</h2>
-            {/* Nested Component START */}
-            {children}
-            <footer>
-                <p>2024 Ecowhite</p>
-            </footer>
-        </div>
-    )
+export default function Container({ children }) {
+  return (
+    <div>
+      <h2>Muhamad Daffa</h2>
+      {/* Nested Component START */}
+      {children}
+      <footer>
+        <p>2024 Ecowhite</p>
+      </footer>
+    </div>
+  );
 }
