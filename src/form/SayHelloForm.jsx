5447f2fb40bc991a66419fdf28b6c6343722a58a
Muhamad Daffa <131875089+dapaii@users.noreply.github.com>
2025-05-24 13:09:12 +0700

    Learn vite/side effect (#18)
    * add Toolbar for event propagation
    
    * add form/ SearchForm for event Default
    
    * add sayHelloForm on form use component sideEffect
    
    * add sayHelloForm on form use component sideEffect
    
    ---------
    
    Co-authored-by: Daffa <mdaffakha@gmail.com>

---
+++ src/form/SayHelloForm.jsx
@@ -0,0 +1,19 @@
+// sideE efffect terjadi di trigger oleh event handler
+export default function SayHelloForm(){
+  // DOM MAIPULATION SECARA MANUAL harusnya pake state
+  function handleClick(e){
+    e.preventDefault();
+    const name = document.getElementById("inputName").value;
+    document.getElementById("textHello").innerText = `Hello ${name}`;
+  }
+
+  return(
+    <div>
+      <form>
+        <input id="inputName"/>
+        <button onClick={handleClick}>Say Hello</button>
+      </form>
+      <h1 id="textHello">Hello World</h1>
+    </div>
+  )
+}
