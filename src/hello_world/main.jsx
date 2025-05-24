24fef50f7ef6e0d4647f3c5a416c129e3afd7962
Muhamad Daffa <131875089+dapaii@users.noreply.github.com>
2025-05-24 12:47:24 +0700

    Learn vite/event propagation (#17)
    * add Toolbar for event propagation
    
    * add form/ SearchForm for event Default
    
    ---------
    
    Co-authored-by: Daffa <mdaffakha@gmail.com>

--- src/hello_world/main.jsx
+++ src/hello_world/main.jsx
@@ -6,6 +6,8 @@ import TodoList from "../../todolist/TodoList";
 import Table from "../../table/Table";
 import AllertButton from "../button/AllertButton";
 import MyButton from "../button/MyButton";
+import Toolbar from "../button/Toolbar";
+import SearchForm from "../form/SearchForm";
 
 // createRoom dibuat untuk memanggil component yang akan dimunculkan
 createRoot(document.getElementById("root")).render(
@@ -18,6 +20,14 @@ createRoot(document.getElementById("root")).render(
       <AllertButton text="click me" message="you click me" />
       <MyButton text="Smash me" onSmash={() => alert("you smash me")}/>
       <MyButton text="kick me" onSmash={() => alert("you kick me")}/>
+
+        <Toolbar onClick={(e) => {
+          // kenapa kita menggunakan e.stopPropagation() karena kita tidak ingin event ini diteruskan ke parent element(jadi tidak 2x memunculkan alert)
+          e.stopPropagation();
+          alert("Toolbar clicked");
+        }}/>
+
+        <SearchForm/>
     </Container>
   </StrictMode>
 );
